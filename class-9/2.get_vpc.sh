25a68af42e6f3a586c3b87068c993e375ffc59fb
unknown <harshavardhan190794@gmail.com>
2024-12-22 15:12:30 +0530

    m

---
+++ class-9/2.get_vpc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+Get_vpc() {
+    echo "Running function to list vpc in Region:$1"
+    get_vpc=$(aws ec2 describe-vpcs --region $1 | jq ".Vpcs[].VpcId" -r)
+    for i in ${get_vpc}; do 
+        echo "The vpcId is $i"
+        echo "====================="
+}
+
