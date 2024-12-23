c7e91eb14bd0ad607b87f9a51dcab783318ac6b9
unknown <harshavardhan190794@gmail.com>
2024-12-23 15:52:52 +0530

    m

--- class-9/4.delete_copy_keypair.sh
+++ class-9/4.delete_copy_keypair.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 pub_key=$( echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7cjS7n/roj4+Vd4w4BGxrbvHP5OkWG1KEQxvxAvTo/gB8M9Ckem/OSacOuEqrkitM/aWVBhL7XD8tGaAZMemKk+8GND21XBKQyh5DTA7XT7K9wov9JXNFT8waj0w1l+KnCJjqDlHffjn/5vrJX3JJPTsfrOFw9upTYixvO5t7esgINDLhKmShS5mBR7lPIr3wBEkXhZ6a+PyYoA8E/csj0rNQSbLQQZ7QFrVprjQFxRY3gRbItomLZQQCfG0s5Ci0jaTt7hMjzReqARVpnaz2sew6Njn1TcoswD/I06NrSOTqlcUPKkaoU8oHt7ToKM9siA3bU29gTAQunSUxF+ch')
-new_pub_key=$(echo $pub_key | tr -d " ")
+new_pub_key=$(echo $pub_key )
 copy_key_pair() { 
    aws ec2 import-key-pair --key-name $1 --public-key-material ${new_pub_key} --region ${region} 
    echo "Key Pair Copied to Region:${region}"
