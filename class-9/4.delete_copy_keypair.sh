aaf10f1c2376b7601eda3f02bb24d96190215ebc
unknown <harshavardhan190794@gmail.com>
2024-12-23 15:06:18 +0530

    m

--- class-9/4.delete_copy_keypair.sh
+++ class-9/4.delete_copy_keypair.sh
@@ -5,7 +5,7 @@ copy_key_pair() {
    aws ec2 import-key-pair --key-name $1 --public-key-material ${pub_key}  --region ${region} 
    }
    
-regions=$(aws ec2 describe-regions | jq ".Regions.[].RegionaName" -r)
+regions=$(aws ec2 describe-regions | jq ".Regions[].RegionaName" -r)
 for region in ${regions}; do 
 if [ ${region} == 'us-east-1' ]; then 
    copy_key_pair ${key_name} ${region} 
