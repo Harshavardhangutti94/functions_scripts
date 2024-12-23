d200be42955f7702c1dd0450447d0be2d6d5464b
unknown <harshavardhan190794@gmail.com>
2024-12-23 12:58:51 +0530

    m

---
+++ class-9/3.delete_vols.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+del_vols() {
+vol_id=$(aws ec2 describe-volumes --region $1 | jq ".volumes[].volumeId" -r)
+for i in ${vol_id}; do 
+   vol_size=$(aws ec2 describe-volumes --volume-ids ${i} --region $1 | jq ".Volumes[].Size" -r)
+   vol_state=$(aws ec describe-volumes --volume-ids ${i} --region $1 |jq ".Volumes[].State" -r)
+    if [[ ${vol_size} -gt 5 -a ${vol_state} == "in-use" ]]; then 
+        echo "Valid Volume.." 
+    else 
+        echo"Invalid volume .... Deleting volume ${i}" 
+        aws ec2 delete-volume --volume-id ${i} --region $1 
+    fi 
+    done 
+    } 
+
+Region=$(aws ec2 describe-regions | jq ".Regions[].RegionName" -r)
+for k in ${Region[@]}; do 
+    del_vols ${k}
+done
