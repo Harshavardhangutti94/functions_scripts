3dcac4187abf0fe98226abaecb8fe4cf4d7f5ce2
unknown <harshavardhan190794@gmail.com>
2024-12-24 13:32:59 +0530

    m

--- class-9/5.break_continue.sh
+++ class-9/5.break_continue.sh
@@ -3,11 +3,10 @@ domain=$@
 if [ $# -gt 0 ]; then 
    for i in $domain; do 
     timeout 3 netcat -zv $i 443 >> /dev/null 2>&1 
-    if [ $? -eq 0] ; then 
+    if [ $? -eq 0 ] ; then 
        echo "$i is up and running"
     else 
        echo "$i id down"
-       continue
     fi 
     done 
 else 
