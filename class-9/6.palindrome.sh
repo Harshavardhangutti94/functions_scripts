9386975cc808c209390ead99f8c7c6fa2f16c2e4
unknown <harshavardhan190794@gmail.com>
2024-12-24 23:57:41 +0530

    m

--- class-9/6.palindrome.sh
+++ class-9/6.palindrome.sh
@@ -2,7 +2,7 @@
 str=$1
 length1=$(echo -n $str | wc -c)
 length2=${#str} 
-for(( i=0; i<$(length2); i++ )); do
+for(( i=0; i<${length2}; i++ )); do
 #for (( i=${length2-1}; i>0 ; i--)); do
      echo ${str[@]:$i:1} 
 done
