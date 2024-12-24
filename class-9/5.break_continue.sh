979abd8d529e6966216fbad09a9ee685d87ba4d3
unknown <harshavardhan190794@gmail.com>
2024-12-24 13:31:11 +0530

    m

--- class-9/5.break_continue.sh
+++ class-9/5.break_continue.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+domain=$@ 
+if [ $# -gt 0 ]; then 
+   for i in $domain; do 
+    timeout 3 netcat -zv $i 443 >> /dev/null 2>&1 
+    if [ $? -eq 0] ; then 
+       echo '$i is up and running' 
+    else 
+       echo '$i id down' 
+    fi 
+    done 
+else 
+    echo "Please provide the server ip to check"
+fi 
