ffb9ef457065fbfe075095c0f9ef7025c8f9817a
unknown <harshavardhan190794@gmail.com>
2024-12-25 00:07:29 +0530

    m

--- class-9/6.palindrome.sh
+++ class-9/6.palindrome.sh
@@ -1,10 +1,11 @@
 #!/bin/bash 
 str=$1
 b=1
+rev=''
 length1=$(echo -n $str | wc -c)
 length2=${#str} 
 #for(( i=0; i<${length2}; i++ )); do
 for (( i=($length2-$b); i>=0 ; i--)); do
-     echo ${str[@]:$i:1} 
+      rev="$rev${str[@]:$i:1}" 
 done
 
