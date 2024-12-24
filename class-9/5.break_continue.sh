a2071a480b07c6c3e2428686b33d214e4c8e104b
unknown <harshavardhan190794@gmail.com>
2024-12-24 14:14:08 +0530

    m

--- class-9/5.break_continue.sh
+++ class-9/5.break_continue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ $# -gt 0 ]; then 
 domain=www.google.com www.facebook.con www.amazon.com www.flipkart.com
+if [ $# -gt 0 ]; then 
    for i in $domain; do 
     timeout 3 netcat -zv $i 443 >> /dev/null 2>&1 
     if [ $? -eq 0 ] ; then 
